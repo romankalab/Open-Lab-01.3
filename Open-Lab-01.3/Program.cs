07697b167cb8e249dd6f3653accb77753fddb850
romankalab <roan.kalab@mail.com>
2019-10-09 19:50:19 +0200

    Initial

---
+++ Open-Lab-01.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Open_Lab_01._1
+{
+    class GFG
+    {
+        public static void Main()
+        {
+            int number;
+
+            Console.WriteLine("Enter your number: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int number1 = number + 1;
+
+            Console.WriteLine($"This is the result: {number1}");
+        }
+    }
+}
